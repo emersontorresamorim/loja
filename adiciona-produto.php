235962bf05a5bd2a5c127831eb6c939585bacf98
Emerson Torres <emerson.torres.tec@gmail.com>
2017-04-07 14:15:44 -0300

    Melhoria do layout, criacao de menu e criacao de funcao para cadastrar produto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,16 +1,21 @@
 <?php include("cabecalho.php") ?>
 	<?php 
+
+		function gravarProduto($conexao, $nome, $preco) {
+			$query = "INSERT INTO produto (nome, preco) VALUES ('{$nome}', {$preco})";
+			return mysqli_query($conexao, $query);
+		}
+
 		$nome = $_GET['nome'];
 		$preco = $_GET['preco'];
-
-		$query = "INSERT INTO produto (nome, preco) VALUES ('{$nome}', {$preco})";
-
 		$conexao = mysqli_connect('localhost:3307', 'root', '', 'loja');
-		if (mysqli_query($conexao, $query)) { ?>
-			<p class="alert-success">O Produto <?= $nome; ?>, no valor de <?= $preco; ?> adicionado com sucesso.</p>
-		<?php } else { ?>
-			<p class="alert-danger">Erro! O Produto não foi adicionado.</p>
-		<?php } 
-		mysqli_close($conexao);
+
+		if (gravarProduto($conexao, $nome, $preco)) { ?>
+			<p class="text-success">O Produto <?= $nome; ?>, no valor de <?= $preco; ?> adicionado com sucesso.</p>
+		<?php } else { 
+			$msg_erro = mysqli_error($conexao); ?>
+			<p class="text-danger">Erro! O Produto não foi adicionado - Motivo: <?= $msg_erro; ?></p>
+		<?php 
+		} 
 	?>
 <?php include("rodape.php") ?>
